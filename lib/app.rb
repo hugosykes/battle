3628046f66e145c044873470d18643dd6698470d
hugosykes <hugo@denmeadfarm.co.uk>
2017-10-11 16:35:29 +0100

    Players can now view each other's hitpoints

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,6 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-
   enable :sessions
 
   get '/' do
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -7,11 +7,20 @@ feature 'Battle' do
     expect(page).to have_content 'Testing infrastructure working!'
   end
 
-  scenario 'players entering names' do
-    visit('/')
-    fill_in :player1, with: 'Ed'
-    fill_in :player2, with: 'Hugo'
-    click_button 'Submit'
-    expect(page.text.scan(/Ed|Hugo/).length).to be 2
+  context 'Ed and Hugo are playing' do
+    before do
+      visit('/')
+      fill_in :player1, with: 'Ed'
+      fill_in :player2, with: 'Hugo'
+      click_button 'Submit'
+    end
+
+    scenario 'players entering names' do
+      expect(page).to have_content 'Ed vs. Hugo'
+    end
+
+    scenario 'checking opponents hit points' do
+      expect(page).to have_content 'Hugo hit points: 0'
+    end
   end
 end
